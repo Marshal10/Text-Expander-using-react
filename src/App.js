bc549cf83a0d02eddc547da5bfdbb16046cd45f5
Marshal10 <marshalmaleane123@gmail.com>
2024-06-06 20:53:06 +0530

    Show or hide text functionality

--- src/App.js
+++ src/App.js
@@ -37,23 +37,33 @@ export default function App() {
 function TextExpander({
   children,
   collapseButtonText = "Show Less",
-  collapsedNumWords,
+  collapsedNumWords = 10,
   expandButtonText = "Show More",
-  buttonColor,
+  buttonColor = "#1f09cd",
   expanded = false,
   className,
 }) {
   const [isExpanded, setIsExpanded] = useState(expanded);
-  const displayText = isExpanded ? children : "test";
+  const displayText = isExpanded
+    ? children
+    : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
+  const style = {
+    color: buttonColor,
+    background: "none",
+    border: "none",
+    marginLeft: "6px",
+    font: "inherit",
+    cursor: "pointer",
+  };
 
   function handletoggleExpand() {
     setIsExpanded((ex) => !ex);
   }
 
   return (
-    <div className={className ? className : ""}>
+    <div className={className}>
       {displayText}
-      <button onClick={handletoggleExpand}>
+      <button onClick={handletoggleExpand} style={style}>
         {isExpanded ? collapseButtonText : expandButtonText}
       </button>
     </div>
